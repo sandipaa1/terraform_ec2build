26a45801118f2c862d876f420546624c3942ac9e
sandipaa1 <123892890+sandipaa1@users.noreply.github.com>
2023-02-02 16:45:36 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,18 +1,23 @@
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-
-  name = "test"
-
-  ami                    = "ami-0574da719dca65348"
-  instance_type          = "t2.micro"
-  key_name               = "Test-keypair"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-0b761f2836106b5e5"]
-  subnet_id              = "subnet-03297e2364a916d16"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIATX62567MPBQGLPM7"
+secret_key = "qKOopdpgtZbq5plMymCZ37/BxvWE3Vt5cv8PDRpG"
 }
+
+resource "aws_instance" "example-server" {
+  ami                         = "ami-0be0a52ed3f231c12" // Ubuntu 20.04 LTS
+  instance_type               = "r5.xlarge"
+  disable_api_termination     = false
+  monitoring                  = false
+  subnet_id                   = "subnet-094f922032637286a"
+  key_name                    = "Terraform_demo"
+  vpc_security_group_ids      = ["sg-015c6bf5272b2ef3f"]
+
+  root_block_device {
+    volume_type           = "standard"
+    volume_size           = 10
+    encrypted             = true
+    delete_on_termination = true
+   }
+  }
+
